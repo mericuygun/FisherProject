d5692b58259948d7d48f16a1781350fbde6c04f6
Meric Uygun <109170558+mericuygun@users.noreply.github.com>
2022-11-03 14:10:59 +0300

    .
    .

---
+++ Assets/Scripts/Catching.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catching : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay(Collision collision);
+    {
+        
+    }
+}
